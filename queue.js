d357038a12521e166fb3bbb3c72071d123840318
Rashed Ahmed Rahat <37572137+rashedrahat@users.noreply.github.com>
2019-05-12 16:08:24 +0600

    Add files via upload

---
+++ queue.js
@@ -0,0 +1,13 @@
+// a program that pushes elements in an array one by one and remove from it according to FIFO.
+
+var books = [];
+
+books.push("Computer Fundamentals");
+books.push("Data Structure");
+books.push("Algorithm");
+
+console.log(books);
+
+books.shift();
+
+console.log(books);
